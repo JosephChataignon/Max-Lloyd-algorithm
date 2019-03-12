2d90e3e79755846365a52288577ccd6fde59a3ca
JosephChataignon <j.chataignon@hotmail.fr>
2019-03-12 14:38:37 +0800

    add verification table of values for 1D, new elements for 2D

--- 2 dimensions/max_lloyd_2D.py
+++ 2 dimensions/max_lloyd_2D.py
@@ -23,42 +23,65 @@ def gaussian(x,y):
 def f(x,y):
     return gaussian(x,y)
 
-# same as f, but the returned value is zero if (x,y) is out of the boundaries
-def f_with_constraints(x,y,boundaries):
-    for b in boundaries:
-        if b[0]*x + b[1]*y > b[2] :
-            return 0
-    return f(x,y)
-
 # distribution studied
 def random_distrib():
     #return [random.uniform(-1,1),random.uniform(-1,1)]
     return [random.gauss(0,1),random.gauss(0,1)]
 
-## not 2-d adapted after this
-# computes MSE inside one region
-def region_MSE(x,germs,boundaries):
-    return integrate.dblquad(lambda x,y: ((t - x)**2) * f(t), t1, t2)[0]# to complete
+# indicator function: returns 1 if x is in the boundaries, 0 otherwise
+def is_part_of_region(x, y, boundaries):
+    for b in boundaries:
+        print b
+        if b[0]*x + b[1]*y > b[2] :
+            return 0
+    return 1
 
-# computes mean square error on R
+# same as f, but the returned value is zero if (x,y) is out of the boundaries
+# here the array boundaries only contains the boundaries for the concerned region
+def f_with_constraints(x,y,boundaries):
+    if is_part_of_region(x, y, boundaries) == 1:
+        return f(x,y)
+    else:
+        return 0
+
+# returns the measure of intersection between the zone inside boundaries and the line x=z
+def Sx(z,boundaries):
+    return integrate.quad(lambda t: is_part_of_region(z,t,boundaries), -float('Inf'), float('Inf'))[0]
+
+# returns the measure of intersection between the zone inside boundaries and the line y=z
+def Sy(z,boundaries):
+    return integrate.quad(lambda t: is_part_of_region(t,z,boundaries), -float('Inf'), float('Inf'))[0]
+
+# computes MSE inside one region
+# here the array boundaries only contains the boundaries for the region of x
+def region_MSE(x,boundaries):
+    y = lambda u,v: f_with_constraints(u,v,boundaries) * ((u-x[0])**2+(v-x[1])**2)
+    return integrate.dblquad( y, -float('Inf'), float('Inf'), -float('Inf'), float('Inf'))[0]
+
+# computes mean squared error on R
 def MSE(germs,boundaries):
     s = 0
     for i in xrange(len(germs)):
-        s = s + interval_MSE(x[i], t[i-1], t[i])
-    return s/len(germs)
+        s = s + region_MSE(germs[i],boundaries[i])
+    return s
 
-# boundaries is an array containing the constraints on the function
+# boundaries is an array containing the constraints for one region
 def centroid(boundaries):
-    if integrate.quad(f, t1, t2)[0] == 0 or t1 == t2:#to complete: if  area == 0
-        return 0
-    else:
-        return integrate.quad(lambda x,y:t*f(t), t1, t2)[0] / integrate.quad(f, t1, t2)[0]#to complete
+    Cx = integrate.quad(lambda z: z*Sx(z,boundaries), -float('Inf'), float('Inf'))[0]
+    Cx = Cx / integrate.quad(lambda z: Sx(z,boundaries), -float('Inf'), float('Inf'))[0]
+    Cy = integrate.quad(lambda z: z*Sy(z,boundaries), -float('Inf'), float('Inf'))[0]
+    Cy = Cy / integrate.quad(lambda z: Sy(z,boundaries), -float('Inf'), float('Inf'))[0]
+    return [Cx,Cy]
+
 
 def adjust_boundaries(germs,k):
     bounds = []
-    main_germ = germs[i]
-    for i in xrange(germs[i]):
-        bounds.append() # to complete
+    main_germ = germs[k]
+    for i in xrange(len(germs)):
+        a1 = 2*( germs[i][0] - main_germ[0] )
+        a2 = 2*( germs[i][1] - main_germ[1] )
+        b  = germs[i][0]**2 + germs[i][1]**2 - main_germ[0] - main_germ[1]
+        bounds.append([a1,a2,b]) 
     return bounds
 
 # boundaries is an array containing the initial decision boundaries (array of n-1 constraints)
@@ -83,18 +106,18 @@ def maxlloyd(boundaries,germs,error_threshold):
         error.append(e)
     return germs,boundaries,error
 
-
+[0,0,0],
 # Test of maxlloyd function
 def test_maxlloyd():
-    b = [-0.5,0,0.5]
-    g = [[0,0],[1,1],[-1,-1]]
-    x2,t2,error = maxlloyd(b,l,0.1)
+    g = [[1,1],[0,0],[2,1],[-1,2],[2,-1],[-4,-4]]
+    b = [[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0]]
+    g2,b2,error = maxlloyd(g,b,0.1)
     print x2,t2
     plt.plot(error)
     plt.show()
     return x2,t2
 
-# test_maxlloyd()
+test_maxlloyd()
 
 def estimate(x,t,value):
     for i in xrange(len(t)):
