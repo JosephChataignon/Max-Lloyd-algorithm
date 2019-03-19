ff109132ff7cf30dffb45860ec46130539f5e79c
JosephChataignon <j.chataignon@hotmail.fr>
2019-03-19 13:58:22 +0800

    add function displaycentroid

--- 2 dimensions/max_lloyd_2D_numerical_approach.py
+++ 2 dimensions/max_lloyd_2D_numerical_approach.py
@@ -18,12 +18,10 @@ def uniform(x,y):
         return 0
 
 def gaussian(x,y):
-    return math.exp((-(x**2)-(y**2))/2)
+    return math.exp((-(float(x)**2.0)-(float(y)**2.0))/2.0)
 
 # function studied
 def f(x,y):
-    if x**2+y**2 > 10:
-        return 0
     return gaussian(x,y)
 
 # distribution studied
@@ -37,27 +35,32 @@ def sqdistance(point1,point2):
 
 
 # computes mean squared error on R
-def MSE(germs,boundaries,grid_dim):
+def MSE(germs,grid):
     print "MSE"
     s = 0
-    for i in xrange(grid_dim[0]):
-        for j in xrange(grid_dim[1]):
-            s = s + sqdistance( [i*10/grid_dim[0]-5,j*10/grid_dim[1]-5] , germs[grid[i][j]] ) / (grid_dim[0]*grid_dim[1])
+    for i in xrange(len(grid)):
+        for j in xrange(len(grid[0])):
+            e = f( float(i)*10.0/float(len(grid))-5.0 , float(j)*10.0/float(len(grid[0]))-5.0 )
+            e = e * sqdistance( [float(i)*10.0/float(len(grid))-5.0,float(j)*10.0/float(len(grid[0]))-5.0] , germs[grid[i,j]] )
+            s = s + e
     return s
 
 # k is the index of the germ to adjust
-def centroid(grid,k,grid_dim):
+def centroid(grid,k):
     print "centroid"
-    Cx = integrate.quad(lambda z: Sx(z,boundaries), -10, 10)[0]
-    if Cx != 0:
-        Cx = integrate.quad(lambda z: z*Sx(z,boundaries), -10, 10)[0] / Cx
+    Cx = 0.0; Cy = 0.0; total_proba = 0.0
+    for i in xrange(len(grid)):
+        for j in xrange(len(grid[i])):
+            if grid[i,j] == k:
+                proba_density = f( float(i)*10.0/float(len(grid))-5.0 , float(j)*10.0/float(len(grid[0]))-5.0 )
+                total_proba = total_proba + proba_density
+                Cx = Cx + proba_density * (float(i)*10.0/float(len(grid))-5.0)
+                Cy = Cy + proba_density * (float(j)*10.0/float(len(grid[0]))-5.0)
+    if total_proba != 0:
+        Cx = Cx / total_proba
+        Cy = Cy / total_proba
     else:
-        Cx = 0
-    Cy = integrate.quad(lambda z: Sy(z,boundaries), -10, 10)[0]
-    if Cy != 0:
-        Cy = integrate.quad(lambda z: z*Sy(z,boundaries), -10, 10)[0] / Cy
-    else:
-        Cy = 0
+        Cx = 0; Cy = 0
     return [Cx,Cy]
 
 #germs is an array containing all the germs
@@ -66,7 +69,7 @@ def adjust_grid(germs,grid_dim):
     grid = np.array([[0]*grid_dim[0]]*grid_dim[1])
     for i in xrange(len(grid)):
         for j in xrange(len(grid[0])):
-            point = [float(i)*10.0/float(grid_dim[0])-5.0 , float(j)*10.0/float(grid_dim[1])-5.0]
+            point = [float(i)*10.0/float(len(grid))-5.0 , float(j)*10.0/float(len(grid[0]))-5.0]
             nearest = 0
             for k in xrange(1,len(germs)):
                 if sqdistance(point,germs[k]) < sqdistance(point,germs[nearest]):
@@ -84,7 +87,7 @@ def maxlloyd(germs,error_threshold,grid_dim):
     e = error_threshold+1
     error = [] # store the evolution of MSE through the loop
     c = 0 # counts the number of executions of the loop
-    grid = [[0]*grid_dim[0]]*grid_dim[1]
+    grid = np.array([[0]*grid_dim[0]]*grid_dim[1])
     while e > error_threshold and c < 10:
         c = c+1
         print c
@@ -96,26 +99,37 @@ def maxlloyd(germs,error_threshold,grid_dim):
             # adjust germs
             print "adjust germs"
             for i in xrange(len(germs)):
-                germs[i] = centroid(grid,i,grid_dim)
-        e = MSE(germs,grid,grid_dim)
+                germs[i] = centroid(grid,i)
+        e = MSE(germs,grid)
         error.append(e)
-    return germs,error
+    return germs,grid,error
 
 
 # Test of maxlloyd function
 def test_maxlloyd():
-    g = [[1,1],[0,0],[2,1],[-1,2],[2,-1],[-4,-4]]
-    g2,error = maxlloyd(g,0.1,[10,10])
-    print g2,b2
-    plt.plot(error)
+    germs = [[1.0,1.0],[-1.0,2.0],[0.0,0.0],[2.0,1.0]]
+    germs,grid,error = maxlloyd(germs,0.1,[15,15])
+    #plt.plot(error)
+    #plt.show()
+    plt.figure(2)
+    for i in xrange(len(grid)):
+        for j in xrange(len(grid[0])):
+            if grid[i][j] == 0:
+                plt.plot(float(i)*10.0/15.0-5.0,float(j)*10.0/15.0-5.0,'ro')
+            elif grid[i][j] == 1:
+                plt.plot(float(i)*10.0/15.0-5.0,float(j)*10.0/15.0-5.0,'go')
+            elif grid[i][j] == 2:
+                plt.plot(float(i)*10.0/15.0-5.0,float(j)*10.0/15.0-5.0,'yo')
+            else:
+                plt.plot(float(i)*10.0/15.0-5.0,float(j)*10.0/15.0-5.0,'bo')
     plt.show()
-    return g2,b2
+    return germs,grid
 
 #test_maxlloyd()
 
 
 
-# test function for visualising the regions of the grid after adjust_grid function
+# test function for visualising the regions of the grid after adjust_grid
 def displayregions():
     germs = [[1.0,1.0],[-1.0,2.0],[0.0,0.0],[2.0,1.0]]
     grid = adjust_grid(germs,[15,15])
@@ -133,9 +147,28 @@ def displayregions():
                 plt.plot(float(i)*10.0/15.0-5.0,float(j)*10.0/15.0-5.0,'bo')
     plt.show()
 
+displayregions()
 
 
+# test function for visualising the centroid of one region
+def displaycentroid():
+    germs = [[1.0,1.0],[-1.0,2.0],[0.0,0.0],[2.0,1.0]]
+    grid = adjust_grid(germs,[15,15])
+    t = 0
+    germs[t] = centroid(grid,t)
+    print germs[t]
+    plt.figure(2)
+    for i in xrange(len(grid)):
+        for j in xrange(len(grid[0])):
+            if sqdistance([i,j] , germs[t]) < 2:
+                plt.plot(float(i)*10.0/15.0-5.0,float(j)*10.0/15.0-5.0,'ro')
+            elif grid[i,j] == t:
+                plt.plot(float(i)*10.0/15.0-5.0,float(j)*10.0/15.0-5.0,'yo')
+            else:
+                plt.plot(float(i)*10.0/15.0-5.0,float(j)*10.0/15.0-5.0,'bo')
+    plt.show()
 
+displaycentroid()
 
 
 
